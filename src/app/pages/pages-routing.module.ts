978e784c5aeea3d8f63c5b221f6b1b7a10739df0
Ahmed Elganaini <ahmed.elganaini@z2data.com>
2024-08-29 15:36:00 +0300

    update

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
 
 const routes: Routes = [{
@@ -21,7 +20,7 @@ const routes: Routes = [{
     {
       path: '**',
       component: NotFoundComponent,
-    },
+    }, 
   ],
 }];
 
